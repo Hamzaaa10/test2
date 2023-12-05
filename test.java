770af2881a5b2d176d34eecb938e7638e6978762
Hamzaaa10 <ah2700749@gmail.com>
2023-12-05 12:53:14 -0800

    add build & desplay steps

--- test.java
+++ test.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkgtry;
 
-/**
- *
- * @author Hamza
- */
-public class Try {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class task {
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("ahmed hamza");
+
+
+        System.out.println(" ahmed hamza ");
+
     }
-    
 }
